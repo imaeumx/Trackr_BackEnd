18b7539939964779a8c2f97ee483b50afdba7f88
vincesju <justinmac9442@gmail.com>
2025-12-09 03:25:58 +0800

    Update email config and .env for Resend integration

--- CineStack/settings.py
+++ CineStack/settings.py
@@ -206,7 +206,7 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() in ('true', '1', 'yes')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'your-verified@resend.com')
 
 # Password Reset Settings
 PASSWORD_RESET_TIMEOUT = 600  # 10 minutes in seconds
